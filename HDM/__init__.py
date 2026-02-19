671258df8ee4f377a5cccf5296ea9a3666a649fd
wush303 <felix@risbro.dk>
2026-02-19 17:21:47 +0100

    Update __init__.py

--- HDM/__init__.py
+++ HDM/__init__.py
@@ -1,13 +1,14 @@
-from .HDM import hdm_embed
-from .utils import HDMConfig, compute_clusters, visualize_by_eigenvector
-
+from .HDM import hdm_run
+from .utils import (
+    HDMConfig,
+    compute_clusters,
+    compute_fiber_kernel_from_maps,
+    visualize_by_eigenvector,
+)
 from .visualization_tools import embed_vs_actual
 
-from .utils import compute_fiber_kernel_from_maps
-
-
 __all__ = [
-    "hdm_embed",
+    "hdm_run",
     "HDMConfig",
     "compute_fiber_kernel_from_maps",
     "compute_clusters",
